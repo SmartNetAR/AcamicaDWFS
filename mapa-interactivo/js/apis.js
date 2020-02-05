3cffdfb2b9d9eb680056179429bc45fd6efa776e
Leonardo Casales <leonardo@smartnet.com.ar>
2020-02-05 16:59:07 -0300

    api key agregada

--- mapa-interactivo/js/apis.js
+++ mapa-interactivo/js/apis.js
@@ -7,7 +7,10 @@ function inicializarMapa () {
     /* Modificá la variable mapa con el constructor Map().
     Tendrás que asignarle un valor de zoom y
     un centro igual a la variable posicionCentral. */
-
+  mapa = new  google.maps.Map( document.getElementById("map"), {
+    center: { lat: -34.456789, lng: -58.123456},
+    zoom: 8
+  })
   geocodificadorModulo.inicializar()
   marcadorModulo.inicializar()
   direccionesModulo.inicializar()

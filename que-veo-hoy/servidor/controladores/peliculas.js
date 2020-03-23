750cd336cf775370dac697a489885124ec40681b
Leonardo Casales <leonardo@smartnet.com.ar>
2020-03-23 19:36:42 -0300

    interpolación sql

--- que-veo-hoy/servidor/controladores/peliculas.js
+++ que-veo-hoy/servidor/controladores/peliculas.js
@@ -3,23 +3,40 @@ const mysql = require('../lib/conexionbd.js');
 exports.mostrarPeliculas = (req, res) => {
 
     const sql = "SELECT * FROM pelicula";
+    const sqlTotalPeliculas = "SELECT COUNT(id) AS total FROM pelicula";
+    const sqlParams = [];
     let where = " WHERE id > 0";
 
-    const {genero, anio, titulo, columna_orden, tipo_orden, pagina, cantidad} = req.query;
+    const {genero, anio, titulo, columna_orden, tipo_orden = 'ASC', pagina, cantidad} = req.query;
 
     if ( genero ) {
-        where = `${where} AND genero_id = ${genero}`
+        where = `${where} AND genero_id = ?`;
+        sqlParams.push(genero);
     }
 
     if ( anio ) {
-        where = `${where} AND anio = ${anio}`
+        where = `${where} AND anio = ?`;
+        sqlParams.push(anio);
     }
 
     if ( titulo ) {
-        where = `${where} AND titulo like '%${titulo}%'`
+        where = `${where} AND titulo like ?`;
+        sqlParams.push(`%${titulo}%`);
     }
 
-    mysql.query(`${sql} ${where}`, ( err, results ) => {
+    const order = columna_orden ? `ORDER BY ${columna_orden} ${tipo_orden}` : '';
+
+    const limit = cantidad ? `LIMIT ${cantidad}` : '';
+
+    let pagination = '';
+    if ( pagina ) {
+        if ( pagina > 1 ) {
+
+            pagination = `OFFSET ${(pagina-1) * cantidad}`
+        }
+    }
+
+    mysql.query(`${sql} ${where} ${order} ${limit} ${pagination}`, sqlParams, ( err, results ) => {
 
         if ( err ) {
             res.status(500).json({
@@ -29,8 +46,29 @@ exports.mostrarPeliculas = (req, res) => {
             })
             throw err;
         }
+        
+        mysql.query(`${sqlTotalPeliculas} ${where}`, sqlParams, ( err, count ) => {
+            if ( err ) {
+                res.status(500).json({
+                    ok: false,
+                    message : "Internal Error",
+                    error: err
+                })
+                throw err;
+            }
 
-        res.json({ peliculas: results });        
+            const total = count[0].total;
+            
+            res.json({
+                ok: true,
+                total,
+                pagina,
+                cantidad,
+                peliculas: results
+            });        
+        });
+        
+
     });
 
 }
